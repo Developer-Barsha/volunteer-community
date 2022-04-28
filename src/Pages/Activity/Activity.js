9ca4901e5bcad6cf1c614978467d400a81952f05
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-28 13:58:14 +0600

    adding ew activity done

--- src/Pages/Activity/Activity.js
+++ src/Pages/Activity/Activity.js
@@ -10,7 +10,7 @@ const Activity = ({ activity }) => {
             <img src={activity.image} alt="" />
 
             <div className="text" style={{ backgroundColor: color}}>
-                <h3>{activity.name}</h3>
+                <h3 style={{textTransform:"capitalize", paddingLeft:'10px'}}>{activity.name}</h3>
             </div>
         </div>
     );
--- src/Pages/AddActivity/AddActivity.js
+++ src/Pages/AddActivity/AddActivity.js
@@ -2,13 +2,31 @@ import React from 'react';
 import './AddActivity.css'
 
 const AddActivity = () => {
+    const handleAddActivity=e=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const image = e.target.image.value;
+        const activity = {name, image};
+        console.log(activity);
+
+        fetch('http://localhost:5000/activities', {
+            method:"POST",
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(activity)
+        })
+        .then(res=>res.json())
+        .then(data=>console.log('data', data))
+    }
+
     return (
         <div className='add-activity'>
             <h2>Add A New Volunteer Activity</h2>
-            <form>
+            <form onSubmit={handleAddActivity}>
                 <input type="text" name='name' placeholder='Activity Name'/>
                 <input type="text" name='image' placeholder='Activity Image'/>
-                <input type="submit" value="Add New Service" />
+                <input type="submit" value="Add New Activity" />
             </form>
         </div>
     );
